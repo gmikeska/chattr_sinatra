a18e05e1427e1d05c029059b043eead2f875ebfa
David Coleman <colemandavid55@gmail.com>
2015-01-30 13:39:57 -0600

    commit before rebase

--- public/javascripts/appBackbone.js
+++ public/javascripts/appBackbone.js
@@ -1,11 +1,13 @@
 var LoginWindow = Backbone.Model.extend({
 	flashMessage: function(msg) {
 		this.set('notification', msg)
+    this.set('username', '')
+    this.set('password', '')
     // flash a message to say your password is wrong and put into the view
     // other portions of this view will not need to be changed dynamically
 	},
   login: function() {
-    client.send({command:'login', username: this.get('username'), password: this.get('password')});
+    client.send({eventName:'login', username: this.get('username'), password: this.get('password')});
   }
 }),
 
@@ -19,9 +21,9 @@ loginWindow = new LoginWindow({
 
 // View for first model below
 var loginTemplateString = '<form>';
-loginTemplateString += '<label>Username</label>'
+loginTemplateString += '<label>Username:</label>'
 loginTemplateString += '<input type="text" name="username" value="<%= username %>"></input><br><br>'
-loginTemplateString += '<label>Password</label>'
+loginTemplateString += '<label>Password:</label>'
 loginTemplateString += '<input type="text" name="password" value="<%= password %>"></input><br><br>'
 loginTemplateString += '<button class="btn btn-primary" id="login-btn"> LOGIN</button>'
 loginTemplateString += '</form>'
@@ -47,10 +49,12 @@ var LoginView = Backbone.View.extend({
     console.log(u);
     this.model.set('username', u);
     this.model.set('password', p);
+    this.model.set('notification', '')
     // grabbing the value of text boxes with our login form
   },
   login: function(e) {
     e.preventDefault();
+
     this.model.login();
   }
 });
@@ -58,6 +62,10 @@ var LoginView = Backbone.View.extend({
 myView = new LoginView({model:loginWindow})
 
 var sampletable = function() {
-    client.ui.newWindow("testwindow2","Another", myView.el)
+    ui.newWindow("testwindow2","Another", myView.el)
     myView.render()
+    client.on('server.error', function(x) {
+      loginWindow.flashMessage("fuck off");
+
+    });
 };
